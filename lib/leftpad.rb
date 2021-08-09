1b73224c00e9425e858104a8460b3947fd72b1e8
manishraj42 <manish.raj@anchanto.com>
2021-08-09 09:15:47 +0530

    first commit

---
+++ lib/leftpad.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "leftpad/version"
+
+module Leftpad
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/leftpad/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Leftpad
+  VERSION = "0.1.0"
+end
